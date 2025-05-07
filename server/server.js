679da2eef1fcc3df5dbbc251b0294cc0a970bea0
Abhinav Gaur <abhinavajgaur@gmail.com>
2025-05-07 18:28:25 +0530

    server changes

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const connectDB =require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+
+
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -9,9 +13,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
+// Connect to MongoDB
+connectDB();
+
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+// API Routes
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
